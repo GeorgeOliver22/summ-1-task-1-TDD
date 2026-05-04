890a699589636032155c1e94d6e81558d789e2bc
George.Oliver <george.oliver@ibm.com>
2026-05-04 17:53:36 +0100

    Red Stage: Added failing test

---
+++ test_logic.py
@@ -0,0 +1,10 @@
+import unittest
+from logic import is_palindrome
+
+class TestPalindrome(unittest.TestCase):
+    def test_simple_palindrome(self):
+        self.assertTrue(is_palindrome("racecar"))
+
+if __name__ == '__main__':
+    unittest.main()
+    
