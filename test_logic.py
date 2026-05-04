47bb074c91fc1ce78ee9a5d6408e3de187e37e21
George.Oliver <george.oliver@ibm.com>
2026-05-04 20:05:14 +0100

    Final Stress Test

--- test_logic.py
+++ test_logic.py
@@ -2,8 +2,15 @@ import unittest
 from logic import is_palindrome
 
 class TestPalindrome(unittest.TestCase):
+
     def test_basic_palindrome(self):
         self.assertTrue(is_palindrome("radar"))
+
+    def test_non_palindrome(self):
+        self.assertFalse(is_palindrome("Manchester"))
+
+    def test_complex_palindromes(self):
+        self.assertTrue(is_palindrome("Kayak"))
 
 if __name__ == '__main__':
     unittest.main()
